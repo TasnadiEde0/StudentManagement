5f672f8fc6b862196ee8fbf974fccd381c60e6ec
edetasnadi <tasnadiede0@gmail.com>
2025-11-04 15:27:13 +0200

    csrf

--- src/main/java/org/learning/studentManagement/configs/SecurityConfigs.java
+++ src/main/java/org/learning/studentManagement/configs/SecurityConfigs.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,14 +30,11 @@ public class SecurityConfigs {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
                                 auth
                                         .requestMatchers("/login").permitAll()
                                         .requestMatchers("/logout").permitAll()
                                         .requestMatchers("/register").permitAll()
-//                                .requestMatchers("/utils/**").hasRole("ADMIN")
-//                                .requestMatchers("/**"). hasRole("USER")
                                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception ->
